cecc32d8283ce4ce6bcda396b5b6da1d1b3c9bae
weiphatttie <117719806+weiphatttie@users.noreply.github.com>
2022-11-08 15:41:18 +0700

    Add files via upload

---
+++ mn g9.py
@@ -0,0 +1,27 @@
+signals = [".",
+           "Weather warning: there is a storm approaching",
+           "~",
+           "Helicopter arriving McMurdo station 10:00 Tuesday",
+           "**",
+           "First aid kit needed at far camp",
+           "*&_)*&^%^&*%^$~@:~",
+           "Food delivery drop will be delayed by 48 hours",
+           "Repairs needed at the observation platform",
+           "Urgent - update all anti-virus systems",
+           "Please re-send meteorological data",
+           "234724u2u23u888",
+           ".."
+           "asjdha## djhaidj# ddjiadj#",
+           "Medical officer requested at main base",
+           " %",
+           "43umcu3rg0ucthgm@:;<",
+           "Penguin migration has begun 2 weeks early",
+           "Solar flare may affect radio communication",
+           "-"]
+good_signals = [] 
+for item in signals: 
+    print(item)
+    good = input("is this a good signal? (Y/N ")
+    if good == "Y"
+         good signals.append (item)
+print(good_signals)
